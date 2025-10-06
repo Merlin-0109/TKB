7cbe5a2deb309aa2a5a4ccd4b6b5dd97e6a1e362
Merlin-0109 <caibutbi2.0@gmail.com>
2025-10-06 18:57:25 +0700

    Update settings.py

--- timetable/timetable/settings.py
+++ timetable/timetable/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'timetable.urls'
@@ -122,3 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
